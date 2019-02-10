0f3e99628f7b4997a020f0b1fe3d918019499642
A Boechko <46904893+Aboechko@users.noreply.github.com>
2019-02-10 09:01:05 -0500

    Returning and mutating
    Excercise showing differences between returning and mutating

---
+++ Returning vs Mutating.js
@@ -0,0 +1,44 @@
+//map
+
+let pets = [
+  {name: "Meowsalot", species: "cat", age: 2},
+  {name: "Barksalot", species: "cat", age: 3},
+  {name: "Purrsloud", species: "cat", age: 8}
+]
+
+console.log(pets.push({name: "Puppster", species: "dog", age:1}))
+let ourTest = pets.map(nameOnly)
+
+function nameOnly(x){
+  return x.name
+}
+
+console.log(ourTest)
+
+//example 2
+
+let pets = [
+  {name: "Meowsalot", species: "cat", age: 2},
+  {name: "Barksalot", species: "cat", age: 3},
+  {name: "Purrsloud", species: "cat", age: 8}
+]
+
+console.log(pets.push({name: "Puppster", species: "dog", age:1}))
+let ourTest = pets.map(nameOnly)
+
+function nameOnly(x){
+  return x.name
+}
+
+let dogs = pets.filter(onlyDogs)
+
+function onlyDogs(x) {
+  return x.species == "dog"
+}
+
+function onlyBabies(x){
+  return x.age < 3
+}
+
+let babyDogNames = pets.filter(onlyDogs).filter(onlyBabies).map(nameOnly)
+console.log(babyDogNames)
